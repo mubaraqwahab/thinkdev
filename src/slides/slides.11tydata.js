bc373f6b97f123b227ea18a8e044b808494fd3a8
Mubaraq Wahab <48601635+mubaraqwahab@users.noreply.github.com>
2021-09-20 11:57:17 +0100

    Compute title frontmatter

--- src/slides/slides.11tydata.js
+++ src/slides/slides.11tydata.js
@@ -1,6 +1,12 @@
 module.exports = {
-  "layout": "layouts/presentation",
+  layout: "layouts/presentation",
   eleventyComputed: {
-    "permalink": (data) => `/lessons/${data.page.fileSlug}/slides/`
+    title: (data) => {
+      `data.collections.lessons`
+      const sn = +data.page.fileSlug.slice(0, 2)
+      const lesson = data.collections.lessons[sn]
+      return `Slide ${sn}: ${lesson.title}`
+    }
+    permalink: (data) => `/lessons/${data.page.fileSlug}/slides/`
   }
 }
