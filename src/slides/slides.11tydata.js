cb5e06b3e703a9089a03a37083862c84dde6d51e
Mubaraq Wahab <48601635+mubaraqwahab@users.noreply.github.com>
2021-09-20 12:35:14 +0100

    ...

--- src/slides/slides.11tydata.js
+++ src/slides/slides.11tydata.js
@@ -4,8 +4,8 @@ module.exports = {
     title: (data) => {
       `data.collections.lessons`
       const sn = +data.page.fileSlug.slice(0, 2)
-      const lesson = data.collections.lesson[sn]
-console.log({ sn, cl: data.collections, ls: data.collections.lesson })
+      const lesson = data.lessons[sn]
+console.log({ sn, cl: data.lessons, lesson })
       return `Slide ${sn}: ${lesson.title}`
     },
     permalink: (data) => `/lessons/${data.page.fileSlug}/slides/`
