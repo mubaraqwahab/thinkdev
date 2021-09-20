c239f87555f45de58245b40fde71a50720655473
Mubaraq Wahab <48601635+mubaraqwahab@users.noreply.github.com>
2021-09-20 12:56:18 +0100

    ...

--- src/slides/slides.11tydata.js
+++ src/slides/slides.11tydata.js
@@ -2,12 +2,10 @@ module.exports = {
   layout: "layouts/presentation",
   eleventyComputed: {
     title: (data) => {
-      `data.collections.lessons`
-      const sn = +data.page.fileSlug.slice(0, 2)
-      const lesson = data.lessons[sn]
-console.log({ sn, cl: data.lessons, lesson })
-      return `Slide ${sn}: ${lesson.title}`
+      const serialNo = data.page.fileSlug.slice(0, 2)
+      const lesson = data.lessons[+serialNo]
+      return `${serialNo} ${lesson.title}`
     },
-    permalink: (data) => `/lessons/${data.page.fileSlug}/slides/`
+    permalink: (data) => `/lessons/${data.title}/slides/`
   }
 }
