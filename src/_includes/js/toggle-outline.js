051391998a2bdae790ebe9acab7b562c6fc536e8
Mubaraq Wahab <mub.wahab26@gmail.com>
2021-10-01 12:23:17 +0100

    change md breakpoint to lg

--- src/_includes/js/toggle-outline.js
+++ src/_includes/js/toggle-outline.js
@@ -1,11 +1,13 @@
 // @ts-check
 
 const toggleBtn = document.getElementById("outlineToggle")
+const toggleIcon = toggleBtn.querySelector(".bi")
 const outlineNav = document.getElementById(
   toggleBtn.getAttribute("aria-controls")
 )
 
 toggleBtn.addEventListener("click", () => {
   toggleBtn.ariaExpanded = toggleBtn.ariaExpanded === "true" ? "false" : "true"
+  toggleIcon.classList.toggle("rotate-90", toggleBtn.ariaExpanded !== "false")
   outlineNav.classList.toggle("hidden", toggleBtn.ariaExpanded === "false")
 })
