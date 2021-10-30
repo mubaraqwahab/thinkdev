bd3cd652bce6345542299c06fff4963ad2b1408f
Mubaraq Wahab <mub.wahab26@gmail.com>
2021-10-30 12:44:06 +0100

    sw lookin good
    alhamdulillah!

---
+++ src/sw.js
@@ -0,0 +1,69 @@
+// TODO: change cache name
+const CACHE_NAME = "v2"
+const OFFLINE_PAGE = "/offline.html"
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll([OFFLINE_PAGE, "/assets/css/main.css"]))
+  )
+  self.skipWaiting()
+})
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    // Delete all previous caches
+    caches.keys().then((keys) =>
+      keys.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key)
+      })
+    )
+  )
+})
+
+self.addEventListener("fetch", (event) => {
+  if (
+    !event.request.url.startsWith("http") ||
+    event.request.method !== "GET" ||
+    // Don't cache browser-sync stuff during development
+    event.request.url.includes("/browser-sync/")
+  ) {
+    return
+  }
+
+  event.respondWith(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME)
+      const requestMode = event.request.mode
+
+      // Returned the cached resource, if any
+      const cachedResource = await cache.match(event.request)
+      if (cachedResource) return cachedResource
+
+      try {
+        // If it hasn't been cached, fetch from server
+        const response = await fetch(event.request)
+
+        // Cache the resource (a clone of the response)
+        // only if it's a good one
+        if (response.ok /*&& response.type !== "opaque"*/) {
+          setTimeout(() => {
+            console.log({ url: response.url, ok: response.ok })
+          }, 5000)
+          await cache.put(event.request, response.clone())
+        }
+
+        // Return the original response to the browser
+        return response
+      } catch (e) {
+        console.error(e)
+        // Show fallback page if the request was for a webpage
+        // and it failed ("failed" as in a network failure)
+        if (requestMode === "navigate") {
+          return cache.match(OFFLINE_PAGE)
+        }
+      }
+    })()
+  )
+})
