f238aaf8d71e019775052726db3f735065733a25
Mubaraq Wahab <mub.wahab26@gmail.com>
2021-11-02 12:40:16 +0100

    add passthrough copy for slides

--- .eleventy.js
+++ .eleventy.js
@@ -56,7 +56,7 @@ module.exports = function (eleventyConfig) {
 
   // Keep directory structure for these
   eleventyConfig.addPassthroughCopy("src/assets/images/")
-  eleventyConfig.addPassthroughCopy("src/lessons/**/slides.pdf")
+  eleventyConfig.addPassthroughCopy("src/slides/*.pdf")
   eleventyConfig.addPassthroughCopy("src/favicon.*")
   eleventyConfig.addPassthroughCopy("src/android-chrome-*.png")
   eleventyConfig.addPassthroughCopy("src/apple-touch-icon.png")
