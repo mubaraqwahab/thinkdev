371332a0844679d0ea794d6f76706fff4dd49fd6
Mubaraq Wahab <mub.wahab26@gmail.com>
2021-11-08 15:42:39 +0100

    cleanup sw

--- src/_includes/js/sw.js
+++ src/_includes/js/sw.js
@@ -3,21 +3,18 @@ const OFFLINE_PAGE = "/offline.html"
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) =>
-      cache.addAll([
-        OFFLINE_PAGE,
-        "/main.css",
-        "/reveal/dist/reveal.css",
-        "/reveal/plugin/highlight/github-dark-dimmed.css",
-        "/reveal/dist/reveal.js",
-        "/reveal/plugin/highlight/highlight.js",
-        // TODO: automate this! (or get rid of it)
-        // "/lessons/01-introduction/",
-        // "/slides/01-introduction/",
-        // "/lessons/02-values-and-types/",
-        // "/slides/02-values-and-types/",
-      ])
-    )
+    caches
+      .open(CACHE_NAME)
+      .then((cache) =>
+        cache.addAll([
+          OFFLINE_PAGE,
+          "/main.css",
+          "/reveal/dist/reveal.css",
+          "/reveal/plugin/highlight/github-dark-dimmed.css",
+          "/reveal/dist/reveal.js",
+          "/reveal/plugin/highlight/highlight.js",
+        ])
+      )
   )
   self.skipWaiting()
 })
