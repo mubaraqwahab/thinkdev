092139598326cc1f42d2dd2d1b3e9bc49f01519e
Mubaraq Wahab <mub.wahab26@gmail.com>
2021-11-08 16:48:32 +0100

    update desc

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: "Think Like a Developer",
   shortTitle: "thinkdev",
-  description: "An introductory course to programming concepts in JavaScript",
+  description: "An introductory course to programming concepts, in JavaScript.",
   author: "Mubaraq Wahab",
   environment: process.env.NODE_ENV,
   repoUrl: "https://github.com/mubaraqwahab/thinkdev",
