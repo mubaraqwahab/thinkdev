0dfb63cd64c42835d76b28d45ef3d58d1a97f651
Mubaraq Wahab <mub.wahab26@gmail.com>
2021-11-10 15:47:34 +0100

    remove rel noopener

--- src/_includes/js/rel-noopener.js
+++
@@ -1,8 +0,0 @@
-// It might be best to do this statically, but ...
-// Add rel="noopener" to all external links
-// (i.e. anchors whose hrefs starts with "http")
-window.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll('a[href^="http"]').forEach((anchor) => {
-    anchor.setAttribute("rel", "noopener")
-  })
-})
