d8fc22f55a288032fa3a4626f9b66b62241bb547
Mubaraq Wahab <mub.wahab26@gmail.com>
2022-03-12 21:26:19 +0100

    isolate prose styles to article
    style the footer and add github link there

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,7 +2,10 @@ module.exports = {
   title: "Think Like a Developer",
   shortTitle: "thinkdev",
   description: "An introductory course to programming concepts, in JavaScript.",
-  author: "Mubaraq Wahab",
+  author: {
+    name: "Mubaraq Wahab",
+    url: "https://github.com/mubaraqwahab",
+  },
   environment: process.env.NODE_ENV,
   repoUrl: "https://github.com/mubaraqwahab/thinkdev",
 }
