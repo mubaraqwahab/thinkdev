b7eec346b45710368925d3c4ad007811880c2fe0
Mubaraq Wahab <mub.wahab26@gmail.com>
2022-03-26 15:06:31 +0100

    update site desc
    "with" not "in"

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "Think Like a Developer",
   shortTitle: "thinkdev",
   description:
-    "Let's relearn the basics of programming in the world's most popular language, JavaScript.",
+    "Let's relearn the basics of programming with the world's most popular language, JavaScript.",
   author: {
     name: "Mubaraq Wahab",
     url: "https://github.com/mubaraqwahab",
