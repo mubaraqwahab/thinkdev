3a5d2dd5ca887f989f80e259a534d5cc26c41626
Mubaraq Wahab <mub.wahab26@gmail.com>
2022-04-18 19:15:10 +0100

    add requiredEnv fn to warn if env var isnt set
    This is useful when you pull the repo newly and you dont have a .env file, like I just did

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 module.exports = {
   title: "Think Like a Developer",
   shortTitle: "thinkdev",
@@ -7,8 +9,21 @@ module.exports = {
     name: "Mubaraq Wahab",
     url: "https://github.com/mubaraqwahab",
   },
-  url: process.env.URL,
-  repoUrl: process.env.REPOSITORY_URL,
-  deployBranch: process.env.HEAD,
+  url: requiredEnv("URL"),
+  repoUrl: requiredEnv("REPOSITORY_URL"),
+  deployBranch: requiredEnv("HEAD"),
   environment: process.env.NODE_ENV,
+}
+
+/**
+ * Use this for required environment variables.
+ * If the variable is not set, it will log a warning.
+ * @param {string} name
+ * @returns {string|void}
+ */
+function requiredEnv(name) {
+  return (
+    process.env[name] ??
+    console.warn(`Warning: Environment variable '${name}' not set.`)
+  )
 }
