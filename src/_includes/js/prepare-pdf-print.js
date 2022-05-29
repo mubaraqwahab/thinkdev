6fac3fb4814baf232b3e391a90d9a23b05dc1daf
Mubaraq Wahab <mub.wahab26@gmail.com>
2022-05-29 14:15:29 +0100

    reduce slides motion when necessary

---
+++ src/_includes/js/prepare-pdf-print.js
@@ -0,0 +1,21 @@
+const windowURL = new URL(window.location.href)
+if (windowURL.searchParams.has("print-pdf")) {
+  // Just so I don't have to manually rename the pdf
+  document.title = "{{ page.fileSlug }}"
+
+  // Rewrite all local anchor URLs to prod site URL when printing.
+  const localAnchors = document.querySelectorAll('a[href^="/"]')
+
+  localAnchors.forEach((anchor) => {
+    const anchorURL = new URL(anchor.href)
+    const url = new URL("{{ site.url }}")
+
+    // Set the url props, except the origin, to the anchor's.
+    url.pathname = anchorURL.pathname
+    url.hash = anchorURL.hash
+    url.search = anchorURL.search
+
+    // Point the href to the prod site.
+    anchor.href = url.href
+  })
+}
---
+++ src/_includes/js/setup-reveal.js
@@ -0,0 +1,30 @@
+const prefersReducedMotion = window.matchMedia(
+  "(prefers-reduced-motion: reduce)"
+)
+const reducedMotionConfig = {
+  transition: "none",
+  autoAnimate: false,
+}
+
+const defaultMotionConfig = {
+  transition: "slide",
+  autoAnimate: true,
+}
+
+// See https://revealjs.com/config/
+Reveal.initialize({
+  hash: true,
+  hashOneBasedIndex: false,
+  navigationMode: "linear",
+  plugins: [RevealHighlight],
+  ...(prefersReducedMotion.matches ? reducedMotionConfig : defaultMotionConfig),
+})
+
+prefersReducedMotion.addEventListener("change", () => {
+  console.log("change")
+  if (prefersReducedMotion.matches) {
+    Reveal.configure(reducedMotionConfig)
+  } else {
+    Reveal.configure(defaultMotionConfig)
+  }
+})
