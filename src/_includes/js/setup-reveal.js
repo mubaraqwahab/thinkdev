e0a2ea81ba47e15c9c897380b5b9c5aae063acce
Mubaraq Wahab <mub.wahab26@gmail.com>
2022-05-31 14:27:19 +0100

    add motionconfig typedef
    this is just to fix the @ts-check error

--- src/_includes/js/setup-reveal.js
+++ src/_includes/js/setup-reveal.js
@@ -3,11 +3,14 @@
 const prefersReducedMotion = window.matchMedia(
   "(prefers-reduced-motion: reduce)"
 )
+
+/** @type {MotionConfig} */
 const reducedMotionConfig = {
   transition: "none",
   autoAnimate: false,
 }
 
+/** @type {MotionConfig} */
 const defaultMotionConfig = {
   transition: "slide",
   autoAnimate: true,
@@ -30,3 +33,7 @@ prefersReducedMotion.addEventListener("change", () => {
     Reveal.configure(defaultMotionConfig)
   }
 })
+
+/**
+ * @typedef {Pick<RevealOptions, "transition"|"autoAnimate">} MotionConfig
+ */
