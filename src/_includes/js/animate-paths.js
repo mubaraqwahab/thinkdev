0c308beb1f6d9dbc5ad630dbacd1837277a798ec
Mubaraq Wahab <mub.wahab26@gmail.com>
2022-06-03 12:57:50 +0100

    make path animation work for multiple paths in a slide

--- src/_includes/js/animate-paths.js
+++ src/_includes/js/animate-paths.js
@@ -1,5 +1,3 @@
-// @ts-check
-
 window.addEventListener("DOMContentLoaded", () => {
   const revealConfig = Reveal.getConfig()
 
@@ -7,25 +5,33 @@ window.addEventListener("DOMContentLoaded", () => {
     const prevSVGNode = e.fromSlide.querySelector("svg")
     const nextSVGNode = e.toSlide.querySelector("svg")
 
-    const prevPathNode = prevSVGNode.querySelector("path[data-id]")
-    const pathId = prevPathNode.dataset.id
-    const nextPathNode = nextSVGNode.querySelector(`path[data-id="${pathId}"]`)
+    if (!prevSVGNode || !nextSVGNode) return
 
-    if (!(prevPathNode && nextPathNode)) return
+    prevSVGNode
+      .querySelectorAll("path[data-id]")
+      .forEach((/** @type {SVGPathElement} */ prevPathNode) => {
+        const pathId = prevPathNode.dataset.id
+        const nextPathNode = nextSVGNode.querySelector(
+          `path[data-id="${pathId}"]`
+        )
+        if (!nextPathNode) return
 
-    const prevPath = SVG(prevPathNode)
-    const nextPath = SVG(nextPathNode)
+        const prevPath = SVG(prevPathNode)
+        const nextPath = SVG(nextPathNode)
 
-    // Disable the unnecessary default transform so that the next
-    // path node starts in the same position as the prev path node.
-    nextPath.node.style.setProperty("transform", "none", "important")
+        // Disable the unnecessary default Revealtransform so that the next
+        // path node starts in the same position as the prev path node.
+        nextPath.node.style.setProperty("transform", "none", "important")
 
-    const prevAttributes = prevPath.attr(["d"])
-    const nextAttributes = nextPath.attr(["d"])
+        const prevAttributes = prevPath.attr(["d"])
+        const nextAttributes = nextPath.attr(["d"])
 
-    nextPath
-      .attr(prevAttributes)
-      .animate({duration: revealConfig.autoAnimateDuration * 1000})
-      .attr(nextAttributes)
+        // console.log({pathId, prevAttributes, nextAttributes})
+
+        nextPath
+          .attr(prevAttributes)
+          .animate({duration: revealConfig.autoAnimateDuration * 1000})
+          .attr(nextAttributes)
+      })
   })
 })
